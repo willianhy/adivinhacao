57a7f3ba1dd5f1da1c4db9ef118091789fac30a6
Willian Yassunaka <willian.yassunaka@benner.com.br>
2017-09-20 13:30:35 -0300

    refactoring...

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,21 +3,27 @@ print("Bem vindo no jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
-chute_str = input("Digite o seu número: ")
-print("Você digitou", chute_str)
+total_de_tentativas = 3
+rodada = 1
 
-chute = int(chute_str)
+while (rodada <= total_de_tentativas):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite o seu número: ")
+    print("Você digitou", chute_str)
+    chute = int(chute_str)
 
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-if acertou:
-    print("Você acertou!")
-else:
-    if maior:
-        print("Você errou! O seu chute foi maior que o número secreto.")
-    elif menor:
-        print("Você errou! O seu chute foi menor que o número secreto.")
+    if acertou:
+        print("Você acertou!")
+    else:
+        if maior:
+            print("Você errou! O seu chute foi maior que o número secreto.")
+        elif menor:
+            print("Você errou! O seu chute foi menor que o número secreto.")
+
+    rodada = rodada + 1
 
 print("Fim do jogo")
