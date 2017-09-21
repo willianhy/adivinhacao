5ee7d72355f535bf7e0b4cfce7e778cbc7109bc7
Willian Yassunaka <willian.yassunaka@benner.com.br>
2017-09-21 13:36:00 -0300

    break/continue

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,13 +4,17 @@ print("*********************************")
 
 numero_secreto = 42
 total_de_tentativas = 3
-rodada = 1
 
-while (rodada <= total_de_tentativas):
+
+for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute_str = input("Digite o seu número: ")
+    chute_str = input("Digite um número entre 1 e 100: ")
     print("Você digitou", chute_str)
     chute = int(chute_str)
+
+    if chute < 1 or chute > 100:
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
@@ -18,12 +22,11 @@ while (rodada <= total_de_tentativas):
 
     if acertou:
         print("Você acertou!")
+        break
     else:
         if maior:
             print("Você errou! O seu chute foi maior que o número secreto.")
         elif menor:
             print("Você errou! O seu chute foi menor que o número secreto.")
-
-    rodada = rodada + 1
 
 print("Fim do jogo")
